8d85c5ff8a048350f732cc7e78145324fe9e6a85
carniiga <fernandezagustin98@.com>
2023-08-10 22:23:26 -0300

    more mb

--- src/server/app.ts
+++ src/server/app.ts
@@ -8,8 +8,8 @@ app.use(cors({
 }))
     
 app.use(express.json())
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
+app.use(express.json({limit: '100mb'}));
+app.use(express.urlencoded({limit: '100mb'}));
 
 
 
