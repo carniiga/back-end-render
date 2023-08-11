8631f6b366bfafca2a356792ad5c6c4120225669
carniiga <fernandezagustin98@.com>
2023-08-10 22:28:22 -0300

    more mb

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,8 +1,21 @@
 import express from "express"
+import  bodyParser  from "body-parser";
+
+
+
 import cors from "cors"
 
 export const port = 3000;
 export const app = express()
+
+app.use(bodyParser.json({limit: '35mb'}));
+app.use(
+    bodyParser.urlencoded({
+      extended: true,
+      limit: '35mb',
+      parameterLimit: 50000,
+    }),
+  );
 app.use(cors({
     origin : "*",
 }))
