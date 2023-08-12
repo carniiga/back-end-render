3797f91c25ae60d2321afb97acdd2d2c21d136d1
carniiga <fernandezagustin98@.com>
2023-08-11 22:13:40 -0300

    new Delete

--- src/logic/product-logic/deleteProd.ts
+++ src/logic/product-logic/deleteProd.ts
@@ -1,7 +1,8 @@
 import Product from "../../model/productModel/productSchema";
 
 export const deleteProd = async(id : any) => {
-  const deleteProd = await Product.findOneAndDelete({id})
+  const id2 = id.trim("");
+  const deleteProd = await Product.findByIdAndDelete(id2)
   if(deleteProd){
     return "el  producto ha sido elimiando correctamente"
   }
